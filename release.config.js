83a3a4d3ff1e875e9e85b91dbb939028cec9931a
Thomas Sloboda <thomas@sloboda.fr>
2024-04-17 11:46:08 +0200

    feat(config): update semantic-release config

--- release.config.js
+++ release.config.js
@@ -3,11 +3,27 @@
  */
 module.exports = {
   branches: ["main"],
-  plugins:[
-    "@semantic-release/commit-analyzer",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "patch", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "feat", release: "patch" },
+          { type: "minor", release: "minor" },
+          { type: "break", release: "major" },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
-    "@semantic-release/git"
-  ]
+    "@semantic-release/git",
+  ],
 };
