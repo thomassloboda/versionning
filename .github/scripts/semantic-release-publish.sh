dc4df1dcb0de2acc23179aaed42959613873c31a
Thomas SLOBODA <thomas@sloboda.fr>
2024-06-25 17:14:02 +0200

    feat(ci): update registry eig

--- .github/scripts/semantic-release-publish.sh
+++ .github/scripts/semantic-release-publish.sh
@@ -10,6 +10,8 @@ export SHA=$( git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT )
 export LAST_COMMIT_SHA=$(git rev-parse origin/$DESTINATION_BRANCH)
 export CONTENT=$( base64 -i $FILE_TO_COMMIT )
 
+echo "/repos/thomassloboda/$REPO/contents/$FILE_TO_COMMIT"
+
 gh api --method PUT /repos/thomassloboda/$REPO/contents/$FILE_TO_COMMIT \
   --field message="$MESSAGE" \
   --field content="$CONTENT" \
@@ -17,6 +19,11 @@ gh api --method PUT /repos/thomassloboda/$REPO/contents/$FILE_TO_COMMIT \
   --field branch="$DESTINATION_BRANCH" \
   --field sha="$SHA"
 
+echo "/repos/thomassloboda/$REPO/git/tags"
 TAG_SHA=$(gh api -X POST /repos/thomassloboda/$REPO/git/tags -f tag="$VERSION" -f message="Tag for version $VERSION" -f object="$LAST_COMMIT_SHA" -f type='commit' | jq -r .sha)
+
+echo "/repos/thomassloboda/$REPO/git/refs"
 gh api -X POST /repos/thomassloboda/$REPO/git/refs -f ref="refs/tags/$VERSION" -f sha="$TAG_SHA"
+
+echo "/repos/thomassloboda/$REPO/git/releases"
 gh api -X POST /repos/thomassloboda/$REPO/releases -f tag_name="$VERSION" -f name="$VERSION" -f body="This is a release of version $VERSION"
